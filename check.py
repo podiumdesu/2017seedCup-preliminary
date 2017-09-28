aeeb8d2360e9b58c1c92b513205f229f726025c5
podiumdesu <wwhmomo@gmail.com>
2017-09-28 09:23:01 +0800

    process all team data

--- check.py
+++ check.py
@@ -3,14 +3,11 @@
 
 import csv
 print 'h'
-
-with open('./test.csv','r') as f:
+print 27 * 19
+with open('./result.csv','r') as f:
     print "hello"
-    f.readline()
     cv = csv.reader(f)
     for line in cv:
-        if (0 == int(line[0])):
-            print "yes"
         print len(line)
-        print line[0]
-        print type(line[0])
+        if (len(line) == 286):
+            print line[285]
